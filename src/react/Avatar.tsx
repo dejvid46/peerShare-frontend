bcf883c0cae54e255279bc87f27f4356a68bcb76
David Dvorak <david.dvorak@prozeta.eu>
2023-09-03 20:39:24 +0200

    added avatar circle

---
+++ src/react/Avatar.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function AvatarsCircle() {
+  return (
+    <div className="absolute h-12 w-12 l-[calc(50%-25px)] t-[calc(50%-25px)] rounded-[50%] border-2 border-solid border-[blue]"></div>
+  );
+}
