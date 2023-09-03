fedd591e2d3bbd4fcf7d37480001ec0c329a022a
David Dvořák <dejviddvorak146@gmail.com>
2023-09-03 20:31:29 +0200

    nice buttons

--- src/react/BottomSendMess.tsx
+++ src/react/BottomSendMess.tsx
@@ -3,23 +3,27 @@ import React from "react";
 
 export default function BottomSendMess() {
   return (
-    <div className="flex flex-row items-center ">
-      <Input
-        className="mr-4"
-        key="outside-left"
-        labelPlacement="outside-left"
-        size="lg"
-        startContent={
-          <div className="pointer-events-none flex items-center">
-            <span className="text-default-500 sm:text-lg text-base font-bold">
-              Mess:{" "}
-            </span>
-          </div>
-        }
-      />
-      <Button color="primary" variant="shadow" className="min-w-5">
-        Send
-      </Button>
+    <div className="fixed bottom-0 my-8 sm:invisible">
+      <div className="flex flex-row items-center ">
+        <Input
+          size="lg"
+          startContent={
+            <div className="pointer-events-none flex items-center">
+              <span className="text-default-500 sm:text-lg text-base font-bold">
+                Mess:{" "}
+              </span>
+            </div>
+          }
+          endContent={
+            <Button variant="faded" className="min-w-5 h-9">
+              Send
+            </Button>
+          }
+        />
+        {/* <Button color="primary" variant="shadow" className="min-w-5">
+          Send
+        </Button> */}
+      </div>
     </div>
   );
 }
--- src/react/RoomInput.tsx
+++ src/react/RoomInput.tsx
@@ -1,25 +1,33 @@
-import { Button, Input } from "@nextui-org/react";
+import { Button, Card, CardBody, Input } from "@nextui-org/react";
 import React from "react";
 
-export default function RoomInpt() {
+interface RoomInptProps {
+  children: JSX.Element
+}
+
+export default function RoomInpt({children}: RoomInptProps) {
   return (
-    <div className="flex flex-row items-center">
+    <div className="flex flex-row items-center mx-2">
       <Input
-        className="mr-4"
-        key="outside-left"
-        labelPlacement="outside-left"
-        size="lg"
-        startContent={
-          <div className="pointer-events-none flex items-center">
-            <span className="text-default-500 sm:text-lg text-base font-bold">
-              Room:{" "}
-            </span>
-          </div>
-        }
-      />
-      <Button color="primary" variant="shadow" className="min-w-5">
-        Join
-      </Button>
+          size="lg"
+          type="number"
+          className="mr-4"
+          startContent={
+            <div className="pointer-events-none flex items-center">
+              <span className="text-default-500 sm:text-lg text-base font-bold">
+                Room:{" "}
+              </span>
+            </div>
+          }
+          endContent={
+            <Button variant="faded" className="min-w-5 h-9">
+              Join
+            </Button>
+          }
+        />
+        <Button  color="primary" variant="shadow" className="min-w-5" startContent={children}>
+          Share
+        </Button>
     </div>
   );
 }
